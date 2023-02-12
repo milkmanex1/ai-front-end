b34372cd4d5053f3b7f2b75196b6e73bc8b9f199
Gabriel Kok <kokgabriel@gmail.com>
2023-02-12 23:51:14 +0800

    didnt change anything

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { logo, ai } from "./assets";
 import { Home, CreatePost } from "./pages";
+import "./App.css";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+      <header className="w-full flex justify-between items-center  sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
         <Link to="/">
           <div className="flex gap-x-2">
             <img src={ai} alt="logo" className="w-14 object-contain" />
@@ -26,7 +27,7 @@ const App = () => {
           Create
         </Link>
       </header>
-      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+      <main className="sm:p-8 px-4 py-8 w-full gradientBackground min-h-[calc(100vh-73px)]">
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/create-post" element={<CreatePost />}></Route>
